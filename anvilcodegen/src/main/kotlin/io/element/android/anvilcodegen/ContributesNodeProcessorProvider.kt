f0ce1d5701cb8730dd0fbf0e2405bc08da9b8e21
dust-zed <1186669164@qq.com>
2025-05-29 12:28:35 +0800

    代码生成处理器

--- anvilcodegen/src/main/kotlin/io/element/android/anvilcodegen/ContributesNodeProcessorProvider.kt
+++ anvilcodegen/src/main/kotlin/io/element/android/anvilcodegen/ContributesNodeProcessorProvider.kt
@@ -1,4 +1,16 @@
 package io.element.android.anvilcodegen
 
-class ContributesNodeProcessorProvider {
+import com.google.devtools.ksp.processing.SymbolProcessor
+import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
+import com.google.devtools.ksp.processing.SymbolProcessorProvider
+
+class ContributesNodeProcessorProvider: SymbolProcessorProvider {
+    override fun create(environment: SymbolProcessorEnvironment): SymbolProcessor {
+        val enableLogging = environment.options["enableLogging"]?.toBoolean() == true
+        return ContributesNodeProcessor(
+            logger = environment.logger,
+            codeGenerator = environment.codeGenerator,
+            config = ContributesNodeProcessor.Config(enableLogging = enableLogging),
+        )
+    }
 }
